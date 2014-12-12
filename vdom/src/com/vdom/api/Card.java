672df602e01fdfa6a3e7b5069c9b0951aa39ad9e
Matt Gardner <mg1@cs.cmu.edu>
2014-12-12 08:11:13 -0500

    Commiting the code into a new repository

---
+++ vdom/src/com/vdom/api/Card.java
@@ -0,0 +1,57 @@
+package com.vdom.api;
+
+import java.io.Serializable;
+
+import com.vdom.core.CardImpl;
+import com.vdom.core.Cards.Type;
+import com.vdom.core.MoveContext;
+
+
+public interface Card extends Serializable {
+	public Type getType();
+
+    public String getName();
+
+    public String getSafeName();
+    
+    public String getExpansion();
+
+    public String getStats();
+
+    public String getDescription();
+
+    public int getCost(MoveContext context);
+
+    public int getCost(MoveContext context, boolean buyPhase);
+
+    public boolean costPotion();
+    
+    public boolean isPrize();
+    
+    public boolean isShelter();
+    
+    public boolean isLooter();
+    
+    public boolean isRuins();
+    
+    public boolean isKnight();
+    
+    public boolean isOverpay();
+    
+    public Integer getId();
+    
+    public void isBought(MoveContext context);
+    
+    public void isTrashed(MoveContext context);
+    
+	public boolean isImpersonatingAnotherCard();
+    public Card behaveAsCard();
+    public CardImpl getControlCard();
+
+    public boolean isTemplateCard();
+    public CardImpl getTemplateCard();
+
+    public CardImpl instantiate();
+    
+    //public void isGained(MoveContext context);
+}
